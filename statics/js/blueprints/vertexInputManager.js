a371112af37d47837828b2255b3fb3a7b951b861
horimori <r.zhang194@gmail.com>
2016-04-16 16:56:53 -0400

    FINALLY! IT'S HERE! THE LONG AWAITED CROSS-WINDOW COMMUNICATION

--- statics/js/blueprints/vertexInputManager.js
+++ statics/js/blueprints/vertexInputManager.js
@@ -1,11 +1,15 @@
 var popups = require('./../statics/js/blueprints/vertexWindowsManager');
 
 $(document).ready (function () {
+	$(window).on('message', function (e) {
+		console.log(e.originalEvent.data);
+	});
+
 	$('#blueprint').on('click', '.insertNodeMenu-item', function () {
 		var attrId = $(this).attr('id');
 		switch(attrId) {
 			case 'variable':
- 				var var_win = popups.variable();
+ 				var var_win = window.open(popups.variable);
 				break;
 		}
 	});
--- statics/js/blueprints/vertexWindowsManager.js
+++ statics/js/blueprints/vertexWindowsManager.js
@@ -1,18 +1,6 @@
 var path = require('path');
-var gui = window.require('nw.gui');
 
 var HTMLS_PG = '../htmls/node-creation-windows/';
 
-var open_var_win = function () {
-	// console.log("OKAY");
-	var var_win = gui.Window.open(path.join(HTMLS_PG, 'variable_create.html'), {
-		'toolbar': true,
-		'min_width': 400,
-		'min_height': 200,
-		'max_width': 400,
-		'max_height': 200
-	});
-	console.log(var_win);
-}
-
-exports.variable = open_var_win;
+var varWindowHTMLPath = path.join(HTMLS_PG, 'variable_create.html');
+exports.variable = varWindowHTMLPath;
