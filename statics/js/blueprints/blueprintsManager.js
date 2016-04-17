9b8ee3b788b824319fc14693e32db9245618bff7
Sciencei <cporesky@gmail.com>
2016-04-16 21:56:49 -0400

    fixed a reversion of the graph

--- statics/js/blueprints/blueprintsManager.js
+++ statics/js/blueprints/blueprintsManager.js
@@ -2,16 +2,36 @@
 var graph = new joint.dia.Graph;
 var paper;
 $(document).ready(function () {
-
-	// setting up paper and graph
-	paper = new joint.dia.Paper({
-		el: $('#blueprint'),
-		width: $('#blueprint').width(),
-		height: $('#blueprint').height(),
-		model: graph,
-		gridSize: 1
-	});
-
+    paper = new joint.dia.Paper({
+        el: $('#blueprint'),
+        width: $('#blueprint').width(), 
+        height: $('#blueprint').height(),
+        gridSize: 1,
+        model: graph,
+        defaultLink: new joint.dia.Link({
+            attrs: { 
+                '.marker-target': { fill: '#ffffff', stroke: '#ffffff', d: 'M 10 0 L 0 5 L 10 10 z' }, 
+                '.connection': { stroke: '#ffffff' }
+            }
+        }),
+        defaultRouter: { name: 'manhattan' },
+        defaultConnector: { name: 'rounded' },
+        perpendicularLinks: true,
+        linkPinning: false,
+        validateConnection: function(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
+            // Prevent linking from input ports.
+            if (magnetS && magnetS.getAttribute('type') === 'input') return false;
+            // Prevent linking from output ports to input ports within one element.
+            if (cellViewS === cellViewT) return false;
+            // Prevent linking to input ports.
+            return magnetT && magnetT.getAttribute('type') === 'input';
+        },
+        validateMagnet: function(cellView, magnet) {
+            // Note that this is the default behaviour. Just showing it here for reference.
+            // Disable linking interaction for magnets marked as passive (see below `.inPorts circle`).
+            return magnet.getAttribute('magnet') !== 'passive';
+        }
+    });
     graph.on('change:position', function(cell, newPosition, opt) {
 
             if (opt.skipParentHandler) return;
@@ -58,7 +78,7 @@ $(document).ready(function () {
                 position: { x: newX, y: newY },
                 size: { width: newCornerX - newX, height: newCornerY - newY }
             }, { skipParentHandler: true });
-        });
+    });
 });
 
 $(document).ready(function () {
